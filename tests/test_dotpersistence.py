faff878cee37a0abcb58dcccead2b23245cd01d4
Viktor Shlapakov <vshlapakov@gmail.com>
2016-01-15 09:33:55 +0300

    Cover with all other tests

--- tests/test_dotpersistence.py
+++ tests/test_dotpersistence.py
@@ -6,15 +6,16 @@ from scrapy.exceptions import NotConfigured
 
 from scrapy_dotpersistence import DotScrapyPersistence
 
+
 class DotScrapyPersisitenceTestCase(TestCase):
 
     def setUp(self):
-        self.mocked_proc = mock.Mock()
+        self.mocked_proc = mock.MagicMock()
         self.mocked_proc.communicate.return_value = ([], None)
         self.mocked_proc.wait.return_value = 0
-        mocked_popen = mock.Mock()
-        mocked_popen.return_value = self.mocked_proc
-        self.patch = mock.patch('subprocess.Popen', mocked_popen)
+        self.mocked_popen = mock.Mock()
+        self.mocked_popen.return_value = self.mocked_proc
+        self.patch = mock.patch('subprocess.Popen', self.mocked_popen)
         crawler_mock = mock.Mock()
         crawler_mock.settings = Settings({
             'DOTSCRAPY_ENABLED': True,
@@ -58,6 +59,52 @@ class DotScrapyPersisitenceTestCase(TestCase):
             'AWS_ACCESS_KEY_ID': 'access-key',
             'AWS_SECRET_ACCESS_KEY': 'secret-key',
         }
+        # short checks that init called _load_data
+        self.assertEqual(
+            self.instance._s3path,
+            's3://test-bucket/test-user/123/dot-scrapy/testspider/')
+        assert self.mocked_popen.called
+
+    def test_load_data(self):
+        mocked_call = mock.Mock()
+        self.instance._call = mocked_call
+
+        self.instance._load_data()
+        s3_path1 = 's3://test-bucket/test-user/123/dot-scrapy/testspider/'
+        self.assertEqual(self.instance._s3path, s3_path1)
+        assert os.path.exists(self.instance._localpath)
+        mocked_call.assert_called_with(
+            ['s3cmd', 'sync', s3_path1, '/tmp/.scrapy'])
+
+        # test other s3_path w/o bucket_folder
+        mocked_call.reset()
+        self.instance._bucket_folder = None
+        self.instance._load_data()
+        s3_path2 = 's3://test-bucket/123/dot-scrapy/testspider/'
+        self.assertEqual(self.instance._s3path, s3_path2)
+        mocked_call.assert_called_with(
+            ['s3cmd', 'sync', s3_path2, '/tmp/.scrapy'])
+
+    def test_store_data(self):
+        mocked_call = mock.Mock()
+        self.instance._call = mocked_call
+        self.instance._store_data()
+        mocked_call.assert_called_with(
+            ['s3cmd', 'sync', '--delete-removed', '/tmp/.scrapy',
+             's3://test-bucket/test-user/123/dot-scrapy/testspider/'])
+
+    def test_call(self):
+        # reset mocks after init-related call
+        self.mocked_popen.reset()
+        self.mocked_proc.reset()
+        self.instance._call(["test", "cmd"])
+        self.mocked_popen.assert_called_with(
+            ['test', 'cmd'], stderr=-2, stdout=-1,
+            env={'HOME': '/home/user',
+                 'AWS_ACCESS_KEY_ID': 'access-key',
+                 'AWS_SECRET_ACCESS_KEY': 'secret-key'})
+        self.mocked_proc.communicate.assert_called_with()
+        self.mocked_proc.wait.assert_called_with()
 
     def tearDown(self):
         self.patch.stop()
