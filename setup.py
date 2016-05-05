3133dbec456e9503f772accdbbff7994c169d82e
Viktor Shlapakov <vshlapakov@gmail.com>
2016-05-05 14:38:02 +0300

    Require s3cmd installed

--- setup.py
+++ setup.py
@@ -20,5 +20,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ],
-    requires=['Scrapy (>=1.0.3)', 's3cmd (>=1.5.2)'],
+    requires=['Scrapy (>=1.0.3)'],
+    install_requires=['s3cmd>=1.6.0'],
 )
