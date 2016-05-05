7b1457bfc0d67ca47ac69b0cd32c3e906c90bbc2
Viktor Shlapakov <vshlapakov@gmail.com>
2016-05-05 14:57:56 +0300

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapy-dotpersistence',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/scrapy-plugins/scrapy-dotpersistence',
     description='Scrapy extension to sync `.scrapy` folder to an S3 bucket',
     long_description=open('README.rst').read(),
